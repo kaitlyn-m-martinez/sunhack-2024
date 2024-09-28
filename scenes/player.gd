449bfe778527a6cdcf6c5e7a71f50691d23e2c10
kaitlyn-m-martinez <kaitlyn.m.martinez@proton.me>
2024-09-28 13:34:06 -0700

    player walk and run

---
+++ scenes/player.gd
@@ -0,0 +1,18 @@
+extends CharacterBody2D
+
+@export var speed = 100
+
+var movement = Vector4.ZERO
+
+func get_input():
+	
+	movement = Input.get_vector("ui_left", "ui_right", "ui_up", "ui_down")
+	movement = movement.normalized()
+	velocity = movement * speed
+	if Input.is_action_pressed("ui_run"):
+		velocity=velocity*2		#run modifier
+	
+	
+func _process(delta):
+	get_input()
+	move_and_slide()
