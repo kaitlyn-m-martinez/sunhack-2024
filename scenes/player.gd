5df22fd8343b5a2a9c1d8d3e25a787d1d26ea654
kaitlyn-m-martinez <kaitlyn.m.martinez@proton.me>
2024-09-28 14:47:59 -0700

    basic collision

--- scenes/player.gd
+++ scenes/player.gd
@@ -5,13 +5,11 @@ extends CharacterBody2D
 var movement = Vector4.ZERO
 
 func get_input():
-	
 	movement = Input.get_vector("ui_left", "ui_right", "ui_up", "ui_down")
 	movement = movement.normalized()
 	velocity = movement * speed
 	if Input.is_action_pressed("ui_run"):
 		velocity=velocity*2		#run modifier
-	
 	
 func _process(delta):
 	get_input()
